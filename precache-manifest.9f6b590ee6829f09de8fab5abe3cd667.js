e6b685bee677d5bbbd2a9df913e39d0b18fb154e
David <david.valenciano@securitas.es>
2021-01-28 14:31:49 +0100

    Updates

---
+++ precache-manifest.9f6b590ee6829f09de8fab5abe3cd667.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06f4627d8c1dfd344953875e63dc063",
+    "url": "/tefysbrands/index.html"
+  },
+  {
+    "revision": "41bd9e05d81e91e4c924",
+    "url": "/tefysbrands/static/css/main.cf06c0fe.chunk.css"
+  },
+  {
+    "revision": "9dc100e2e375955d1612",
+    "url": "/tefysbrands/static/js/2.bef2f590.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/tefysbrands/static/js/2.bef2f590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41bd9e05d81e91e4c924",
+    "url": "/tefysbrands/static/js/main.5072376d.chunk.js"
+  },
+  {
+    "revision": "aae0bd2d0d7c69d6753d",
+    "url": "/tefysbrands/static/js/runtime-main.0cf27aa2.js"
+  },
+  {
+    "revision": "c402ae28b4d6a25be089d37843265ebc",
+    "url": "/tefysbrands/static/media/arrow1.c402ae28.svg"
+  },
+  {
+    "revision": "16c83e71464603a18dc272190cab4e45",
+    "url": "/tefysbrands/static/media/arrowW.16c83e71.svg"
+  },
+  {
+    "revision": "250acdf5d35bd56da7fc3ee2808f037d",
+    "url": "/tefysbrands/static/media/bgFooter.250acdf5.svg"
+  },
+  {
+    "revision": "2c9e6be776c1abd4684a604aa764d5ed",
+    "url": "/tefysbrands/static/media/bgFooter.2c9e6be7.png"
+  },
+  {
+    "revision": "95ddabba2daba92c01d2f5e9b3723c23",
+    "url": "/tefysbrands/static/media/box.95ddabba.svg"
+  },
+  {
+    "revision": "e57893c9909900fd010782878c7289fb",
+    "url": "/tefysbrands/static/media/checktransCustomers.e57893c9.jpg"
+  },
+  {
+    "revision": "f300a7e073dbd532cec316dc2f24dd2c",
+    "url": "/tefysbrands/static/media/fb.f300a7e0.svg"
+  },
+  {
+    "revision": "12fe3c7b5cafd08732d5235c0230cc7f",
+    "url": "/tefysbrands/static/media/feather.12fe3c7b.svg"
+  },
+  {
+    "revision": "20b90a2a28995921e89318f0ee6b1fa7",
+    "url": "/tefysbrands/static/media/ig.20b90a2a.svg"
+  },
+  {
+    "revision": "e93f1736d2021bde71a004d85033dcb0",
+    "url": "/tefysbrands/static/media/img1.e93f1736.svg"
+  },
+  {
+    "revision": "84fdf437089e00f34f7ea026b67b6eda",
+    "url": "/tefysbrands/static/media/img2.84fdf437.svg"
+  },
+  {
+    "revision": "0664e24d93dfcf5fd577a972140e97a0",
+    "url": "/tefysbrands/static/media/mainImg.0664e24d.svg"
+  },
+  {
+    "revision": "abb73c3fd77d84b4edcd5f4c34d08a48",
+    "url": "/tefysbrands/static/media/mainImg.abb73c3f.png"
+  },
+  {
+    "revision": "db3540230301e5093a24e1c2b3b5aa26",
+    "url": "/tefysbrands/static/media/pc.db354023.svg"
+  },
+  {
+    "revision": "becf886db6a4bf3e90c89a9ccc927b81",
+    "url": "/tefysbrands/static/media/profilePhoto.becf886d.png"
+  },
+  {
+    "revision": "03f9990ac429461863eccd4486ec3132",
+    "url": "/tefysbrands/static/media/sherpaCustomers.03f9990a.jpg"
+  },
+  {
+    "revision": "5bcb937aa969491f914f56e53f3cb000",
+    "url": "/tefysbrands/static/media/supraCustomers.5bcb937a.jpg"
+  },
+  {
+    "revision": "1a2900e1276a2d72cf8be6e4c78ea3fb",
+    "url": "/tefysbrands/static/media/whiteLogo.1a2900e1.svg"
+  },
+  {
+    "revision": "7b519d558d0678f82e8409c1474944ad",
+    "url": "/tefysbrands/static/media/yellowLogo.7b519d55.svg"
+  }
+]);
